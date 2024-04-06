746253ff15cb2c9d5fd4454c0585a772d6e5eddd
Muhammad Husein Jafari <mhuseinj@Muhammads-MacBook-Air.local>
2024-04-06 09:44:53 +0700

    implement logic for math engine

--- pkg/math_engine/math_engine_impl.go
+++ pkg/math_engine/math_engine_impl.go
@@ -1,51 +1,34 @@
 package math_engine
 
-func (m MathEngineForCalculator) Add(x float64, y float64) float64 {
-	//TODO implement me
-	panic("implement me")
-}
+import "math"
 
-func (m MathEngineForCalculator) Multiply(x float64, y float64) float64 {
-	//TODO implement me
-	panic("implement me")
+func (m *MathEngineForCalculator) Add(x float64, y float64) float64 {
+	return x + y
 }
-
-func (m MathEngineForCalculator) Subtract(x float64, y float64) float64 {
-	//TODO implement me
-	panic("implement me")
+func (m *MathEngineForCalculator) Multiply(x float64, y float64) float64 {
+	return x * y
 }
-
-func (m MathEngineForCalculator) Divide(x float64, y float64) float64 {
-	//TODO implement me
-	panic("implement me")
+func (m *MathEngineForCalculator) Subtract(x float64, y float64) float64 {
+	return x - y
 }
-
-func (m MathEngineForCalculator) Abs(x float64) float64 {
-	//TODO implement me
-	panic("implement me")
+func (m *MathEngineForCalculator) Divide(x float64, y float64) float64 {
+	return x / y
 }
-
-func (m MathEngineForCalculator) Neg(x float64) float64 {
-	//TODO implement me
-	panic("implement me")
+func (m *MathEngineForCalculator) Abs(x float64) float64 {
+	return math.Abs(x)
 }
-
-func (m MathEngineForCalculator) Sqrt(x float64) float64 {
-	//TODO implement me
-	panic("implement me")
+func (m *MathEngineForCalculator) Neg(x float64) float64 {
+	return -x
 }
-
-func (m MathEngineForCalculator) Sqr(x float64) float64 {
-	//TODO implement me
-	panic("implement me")
+func (m *MathEngineForCalculator) Sqrt(x float64) float64 {
+	return math.Sqrt(x)
 }
-
-func (m MathEngineForCalculator) Cubert(x float64) float64 {
-	//TODO implement me
-	panic("implement me")
+func (m *MathEngineForCalculator) Sqr(x float64) float64 {
+	return x * x
 }
-
-func (m MathEngineForCalculator) Cube(x float64) float64 {
-	//TODO implement me
-	panic("implement me")
+func (m *MathEngineForCalculator) Cubert(x float64) float64 {
+	return math.Cbrt(x)
+}
+func (m *MathEngineForCalculator) Cube(x float64) float64 {
+	return x * x * x
 }
