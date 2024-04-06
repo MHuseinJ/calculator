3781295faa5f527e0dd61c1d07c5bfca49996f99
Muhammad Husein Jafari <mhuseinj@Muhammads-MacBook-Air.local>
2024-04-06 10:04:11 +0700

    implement main program

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"calculator/pkg/calc_engine"
+	"calculator/pkg/math_engine"
+	"calculator/pkg/util"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Welocome to simple calculator")
+	utilCalc := util.NewUtil()
+	engine := math_engine.NewMathEngineForCalculator()
+	calc := calc_engine.NewCal(engine, utilCalc)
 	input1 := ""
+	var err error
+
 	for input1 != "exit" {
 		fmt.Print(">")
-		_, err := fmt.Scanf("%s", &input1)
+		input1, err = utilCalc.ReadInputString()
 		if err != nil {
 			fmt.Println("sorry, invalid command")
+		}
+		err = calc.ReadInput(input1)
+		if err != nil {
+			fmt.Println("sorry, invalid argument")
 		}
 	}
 }
